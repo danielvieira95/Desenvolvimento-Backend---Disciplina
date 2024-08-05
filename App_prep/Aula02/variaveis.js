57ef649289c7e5c48bcfadec086cfa72e3feaecc
danielvieira95 <engdanielvieira95@gmail.com>
2024-08-05 15:51:53 -0300

    input

--- App_prep/Aula02/variaveis.js
+++ App_prep/Aula02/variaveis.js
@@ -1,4 +1,15 @@
 const idade = 28;
 console.log("Sua idade é ",idade);
 const nome = "Daniel";
-console.log(nome);
+console.log(nome); // imprimir o nome na tela
+// declarar variaveis com const
+// operações aritmeticas
+console.log("Operações aritméticas");
+console.log(10 + (8*2));
+console.log((10+8)*2);
+// conversao de tipos
+console.log(parseInt("2") + parseInt ("2"));
+console.log("5" / "2");
+console.log(`Meu nome é ${nome}`);
+let dado = prompt("Digite seu nome ");
+console.log("Olá" + dado);
