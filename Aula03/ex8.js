c31a1c55f7a26ae1a128aaf1b26ce3378b0fc827
danielvieira95 <engdanielvieira95@gmail.com>
2024-08-14 22:54:43 -0300

    exemplos

---
+++ Aula03/ex8.js
@@ -0,0 +1,16 @@
+const n1 = 4;
+const n2 = 5;
+const n3 = 0;
+//let media = (n1+n2+n3)/3
+media = 4
+console.log(media)
+if(media>=4 && media<=5){
+    console.log("Exame");
+}
+if(media<4){
+    console.log("Reprovado")
+
+}
+else{
+    console.log("aprovado")
+}
