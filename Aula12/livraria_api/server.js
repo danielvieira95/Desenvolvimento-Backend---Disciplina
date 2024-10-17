87e40a752632680585827b9e248c5641d60dc78e
danielvieira95 <engdanielvieira95@gmail.com>
2024-10-16 22:32:51 -0300

    aula12

--- Aula12/livraria_api/server.js
+++ Aula12/livraria_api/server.js
@@ -25,7 +25,7 @@ mongoose.connect('mongodb+srv://danielvieira2006:da2576896@cluster0.j1d4z.mongod
 // Importação das rotas
 const bookRoutes = require('./Routes/books');
 app.use('/api/books',bookRoutes); // irá retornar a rota dos livros
-app.use('/api/auth',authRoutes);
+//app.use('/api/auth',authRoutes);
 // Define a porta do servidor
 app.listen(5000,()=>{
     console.log('Servidor executando na porta 5000');
