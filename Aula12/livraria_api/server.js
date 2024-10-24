433a5bf3ccd8bc4bd26f2c3fd31c2936c7fa2be0
danielvieira95 <engdanielvieira95@gmail.com>
2024-10-23 21:21:51 -0300

    teste express vercel

--- Aula12/livraria_api/server.js
+++ Aula12/livraria_api/server.js
@@ -27,6 +27,7 @@ const bookRoutes = require('./Routes/books');
 app.use('/api/books',bookRoutes); // irá retornar a rota dos livros
 //app.use('/api/auth',authRoutes);
 // Define a porta do servidor
-app.listen(5000,()=>{
+/*app.listen(5000,()=>{
     console.log('Servidor executando na porta 5000');
-});
+});*/
+module.exports = app
